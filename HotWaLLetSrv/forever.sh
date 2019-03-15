dcb372a2a0edcf8c214dfa859611e7565478ac8a
Laewook Lee <lwlee@markany.com>
2019-03-15 11:55:31 +0900

    -

--- HotWaLLetSrv/forever.sh
+++ HotWaLLetSrv/forever.sh
@@ -6,6 +6,9 @@ cd ${SCRIPTPATH}
 
 NOW=`date +%y%m%d-%H%M%S`
 
+echo '> stop all forever process'
 forever stopall
+echo '> list forever process'
 forever list
+echo '> start hot wallwt server'
 forever start -p ${SCRIPTPATH} -l ./log/${NOW}.log -a ./app.js
