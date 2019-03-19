e4b0c9a21086539321e531cc8e732fe7ece10eca
Laewook Lee <lwlee@markany.com>
2019-03-19 13:49:06 +0900

    -

--- ScriptS/SampLeS/v3_to_privatekey.js
+++ ScriptS/SampLeS/v3_to_privatekey.js
@@ -1,28 +1,22 @@
 var ethWaLLet = require('ethereumjs-wallet');
 const readLine = require('readline');
 
-
-var PrivateKey = Buffer.from('efca4cdd31923b50f4214af5d2ae10e7ac45a5019e9431cc195482d707485378', 'hex');
-var WaLLet = ethWaLLet.fromPrivateKey(PrivateKey);
-var UTC = WaLLet.toV3String('password')
-console.log('utc: ' + UTC)
-
-
-const AliceWallet = ethWaLLet.fromV3(UTC, 'password')
-const AlicePrivateKey = AliceWallet.getPrivateKeyString()
-console.log('alice\'s private key: ' + AlicePrivateKey)
-
-
-
-
-const rl = readLine.createInterface({
+const RL = readLine.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-rl.question('What do you think of Node.js? ', (answer) => {
-  // TODO: Log the answer in a database
-  console.log(`Thank you for your valuable feedback: ${answer}`);
+async function main(){
+  RL.question('input v3\n>', (v3) => {
+    var V3 = v3
 
-  rl.close();
-});
+    RL.question('input password\n>', (password) => {
+      var WaLLet = ethWaLLet.fromV3(V3, password)
+      var PrivateKey = WaLLet.getPrivateKeyString()
+      console.log(PrivateKey)
+      RL.close()
+    })
+  })
+}
+
+main()
