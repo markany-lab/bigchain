f7456e05610719c5218e058d605612bc32181d6e
Laewook Lee <lwlee@markany.com>
2019-03-19 15:19:04 +0900

    -

--- ScriptS/SampLeS/privatekey_to_v3.js
+++ ScriptS/SampLeS/privatekey_to_v3.js
@@ -1,23 +1,27 @@
-var ethWaLLet = require('ethereumjs-wallet');
-const readLine = require('readline');
+var ethWaLLet = require('ethereumjs-wallet')
+const readLine = require('readline')
 
 const RL = readLine.createInterface({
   input: process.stdin,
   output: process.stdout
-});
+})
 
 async function main(){
-  RL.question('input private key\n>', (private_key) => {
-    var PrivateKey = private_key.replace('0x', '')
-    PrivateKey = Buffer.from(PrivateKey, 'hex')
-    var WaLLet = ethWaLLet.fromPrivateKey(PrivateKey)
+  var PrivateKey = await new Promise(resolve=>RL.question('input private key\n>', private_key=>{
+    resolve(private_key)
+  }))
 
-    RL.question('input password\n>', (password) => {
-      var V3 = WaLLet.toV3String(password)
-      console.log(V3)
-      RL.close()
-    })
-  })
+  var Password = await new Promise(resolve=>RL.question('input password\n>', password=>{
+    resolve(password)
+  }))
+
+  RL.close()
+
+  PrivateKey = PrivateKey.replace('0x', '')
+  PrivateKey = Buffer.from(PrivateKey, 'hex')
+  var WaLLet = ethWaLLet.fromPrivateKey(PrivateKey)
+  var V3 = WaLLet.toV3String(Password)
+  console.log(V3)
 }
 
 main()
--- ScriptS/SampLeS/v3_to_privatekey.js
+++ ScriptS/SampLeS/v3_to_privatekey.js
@@ -1,22 +1,25 @@
-var ethWaLLet = require('ethereumjs-wallet');
-const readLine = require('readline');
+var ethWaLLet = require('ethereumjs-wallet')
+const readLine = require('readline')
 
 const RL = readLine.createInterface({
   input: process.stdin,
   output: process.stdout
-});
+})
 
 async function main(){
-  RL.question('input v3\n>', (v3) => {
-    var V3 = v3
+  var V3 = await new Promise(resolve=>RL.question('input v3\n>', v3=>{
+    resolve(v3)
+  }))
 
-    RL.question('input password\n>', (password) => {
-      var WaLLet = ethWaLLet.fromV3(V3, password)
-      var PrivateKey = WaLLet.getPrivateKeyString()
-      console.log(PrivateKey)
-      RL.close()
-    })
-  })
+  var Password = await new Promise(resolve=>RL.question('input password\n>', password=>{
+    resolve(password)
+  }))
+
+  RL.close()
+
+  var WaLLet = ethWaLLet.fromV3(V3, Password)
+  var PrivateKey = WaLLet.getPrivateKeyString()
+  console.log(PrivateKey)
 }
 
 main()
