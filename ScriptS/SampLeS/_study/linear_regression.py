4943df529f41228f2744a07966b2434845298167
Laewook Lee <lwlee@markany.com>
2019-03-22 11:49:20 +0900

    -

--- ScriptS/SampLeS/_study/linear_regression.py
+++ ScriptS/SampLeS/_study/linear_regression.py
@@ -1,22 +1,35 @@
 import tensorflow as tf
 
+# X, Y축에 대한 학습 데이타
 DataX = [1,2,3,4,5,6,7]
 DataY = [25000,55000,75000,11000,128000,155000,180000]
+
+# X, Y축에 대한 데이타 영역(?)을 설정하는 것 같음
 PhX=tf.placeholder(tf.float32)
 PhY=tf.placeholder(tf.float32)
+
+# Y=AX+B에 대찬 초기값(랜덤)
 A=tf.Variable(tf.random_uniform([1], -100, 100))
 B=tf.Variable(tf.random_uniform([1], -100, 100))
-H = A * PhX + B
-Cost = tf.reduce_mean(tf.square(H - PhY))
+
+H = A * PhX + B # 가설식(1차방정식)... 이런 식으로 설정 가능하다는 것이 신기함
+
+# 경사하강 알고리즘의 점프 간격
 P = tf.Variable(0.01)
+
+# 경사하강API 사용
 Optimizer = tf.train.GradientDescentOptimizer(P)
+Cost = tf.reduce_mean(tf.square(H - PhY)) # 비용합수 정의, (예측값-실측값)^2 들의 평균
 Train = Optimizer.minimize(Cost)
 init = tf.global_variables_initializer()
+
 Sess = tf.Session()
 Sess.run(init)
+
 for i in range(5001):
     Sess.run(Train, feed_dict={PhX: DataX, PhY: DataY})
     if i % 500 == 0:
         print(i, Sess.run(Cost, feed_dict={PhX: DataX, PhY: DataY}), Sess.run(A), Sess.run(B))
 
+# X축 8, 10 에 대한 Y축 예상치
 print(Sess.run(H, feed_dict={PhX: [8, 10]}))
